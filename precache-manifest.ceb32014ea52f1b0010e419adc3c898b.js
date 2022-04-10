a8ba244f9bd33b131d645736d2eecfb453e90283
vberberidis <vberberidis@netcompany-intrasoft.com>
2022-04-10 16:05:50 +0300

    Updates

---
+++ precache-manifest.ceb32014ea52f1b0010e419adc3c898b.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b83c4e308574a819cab642cd4c609be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e001f32bd5b4e434860",
+    "url": "/static/css/main.19b37cd1.chunk.css"
+  },
+  {
+    "revision": "dfee3039c08d91df4e9c",
+    "url": "/static/js/2.dad257ee.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/static/js/2.dad257ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e001f32bd5b4e434860",
+    "url": "/static/js/main.a0630c1a.chunk.js"
+  },
+  {
+    "revision": "86270678e9ad3edc9349",
+    "url": "/static/js/runtime-main.00060668.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "71eef4be1044ef6c791cd1a27f13be2d",
+    "url": "/static/media/CFC.71eef4be.jfif"
+  },
+  {
+    "revision": "c68e2fd260c416f3b6b6fc80a3c0e74e",
+    "url": "/static/media/GCS.c68e2fd2.jfif"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "564769e72bb34a585182478bbf177912",
+    "url": "/static/media/HakinCodes.564769e7.jfif"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2122f7521824b990f3d2bc10a2ab22ff",
+    "url": "/static/media/NSS.2122f752.jfif"
+  },
+  {
+    "revision": "dfed8eb4a2e838063a8fbd3c29add010",
+    "url": "/static/media/NTL.dfed8eb4.jfif"
+  },
+  {
+    "revision": "17e91d0f56ee82756559340f3d726a49",
+    "url": "/static/media/Olympiad.17e91d0f.png"
+  },
+  {
+    "revision": "065b8c91cc6c2c80dc1c3a66c88b4a6d",
+    "url": "/static/media/Reply.065b8c91.jfif"
+  },
+  {
+    "revision": "bc2e996510c6446bd81b71e65a5ef8fd",
+    "url": "/static/media/SRM_Logo.bc2e9965.png"
+  },
+  {
+    "revision": "b1a89dea3269071962ab719288fa4c04",
+    "url": "/static/media/Screenshot_1.b1a89dea.png"
+  },
+  {
+    "revision": "ce7c334c01d481be0fdf69c7915ff4cd",
+    "url": "/static/media/Screenshot_1_2.ce7c334c.png"
+  },
+  {
+    "revision": "dfdfd9292fa85d49cbaa7ac6a7658b40",
+    "url": "/static/media/Technocolabs.dfdfd929.jfif"
+  },
+  {
+    "revision": "cdc44f0a3266ff6cf3539166738fa24a",
+    "url": "/static/media/Unipi-logo.cdc44f0a.png"
+  },
+  {
+    "revision": "c0daffcaf5c384abec1a49cbdb1602c1",
+    "url": "/static/media/Webarch.c0daffca.jfif"
+  },
+  {
+    "revision": "4338f48a05f91bceb6d1dfe256ecc994",
+    "url": "/static/media/about.4338f48a.txt"
+  },
+  {
+    "revision": "4d9eda58c29877787136f7efd1a2ddcb",
+    "url": "/static/media/android-chrome-512x512.4d9eda58.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "abf387a6bdfc6288ddfb6c18f12c081e",
+    "url": "/static/media/favicon.abf387a6.ico"
+  },
+  {
+    "revision": "d4e222d39f923ba679cb3fbd3dc5ed5c",
+    "url": "/static/media/hackathon1.d4e222d3.png"
+  },
+  {
+    "revision": "45224d44ad7d4507871ec78ecf38832b",
+    "url": "/static/media/hackathon2.45224d44.png"
+  },
+  {
+    "revision": "bc5cfd3a887013976863247e66c389a2",
+    "url": "/static/media/mlh-logo.bc5cfd3a.svg"
+  },
+  {
+    "revision": "53dd27a8801e3a4216b9c9e217dfc7d0",
+    "url": "/static/media/mlh.53dd27a8.png"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "/static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "570644805c99d343f11fa4847f07ff76",
+    "url": "/static/media/netcompany.57064480.jpg"
+  },
+  {
+    "revision": "5be9f97757ab94de789f0a3c2f502e02",
+    "url": "/static/media/profile_photo.5be9f977.jpeg"
+  },
+  {
+    "revision": "2a2870aec103aeab0b5c24916eb2f113",
+    "url": "/static/media/profile_photo_2.2a2870ae.jpeg"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/static/media/site.053100cb.webmanifest"
+  }
+]);
